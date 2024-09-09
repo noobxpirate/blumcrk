58a75a8bcfec736906cba8f2b96713b0e8515018
Anurag Dev <anurag.kumar25@s.amity.edu>
2024-09-09 17:35:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -369,10 +369,12 @@ class blumcrk:
 
     def main(self):
         banner = f"""
-{magenta}??????  ??    •      {putih}Blumcrk Auto Claim for {hijau}blum
-{magenta}??????  ???????????  {hijau}Author : {putih}Noob Pirate
-{magenta}??????  ??? ???? ??  {putih}Github : {hijau}https://github.com/noobxpirate
-{magenta}              ?      {hijau}Note : {putih}Every Action Has a Consequence
+███╗   ██╗ ██████╗  ██████╗ ██████╗     {putih}Blumcrk Auto Claim for {hijau}blum
+████╗  ██║██╔═══██╗██╔═══██╗██╔══██╗  {hijau}Author : {putih}Noob Pirate
+██╔██╗ ██║██║   ██║██║   ██║██████╔╝   {putih}Github : {hijau}https://github.com/noobxpirate
+██║╚██╗██║██║   ██║██║   ██║██╔══██╗   {hijau}Note : {putih}Every Action Has a Consequence
+██║ ╚████║╚██████╔╝╚██████╔╝██████╔╝  {putih}Chat : {hijau}https://t.me/piratexchat 
+╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚═════╝ 
         """
         arg = argparse.ArgumentParser()
         arg.add_argument(
